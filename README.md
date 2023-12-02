c606ad00ff8e30cfe5f42a269594b019b1646032
viiveli <veli.viisanen@pm.me>
2023-12-02 12:59:14 +0200

    day01

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Advent of Code 2023
 ## https://adventofcode.com/2023/
-Second Advent of Code for me. Again with TypeScript, and the goal is to beat last years result of 22 stars
+Second Advent of Code for me. Again with TypeScript
 
 > ### day 01
 > https://adventofcode.com/2023/day/1  
 > - **[./src/day01.ts](<./src/day01.ts> "day 01 solution")**  
+>
--- src/day01.ts
+++ src/day01.ts
@@ -1,3 +1,50 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
-let INPUT = readFileSync('./inputs/day01.in', 'utf8')
+let INPUT = readFileSync("./inputs/day01.in", "utf8");
+let TEST = readFileSync("./inputs/day012.test", "utf8");
+
+const wordMap = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+const pattern = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g;
+
+const solver = (input: string) => {
+  let sum = 0;
+
+  for (const row of input.split("\n")) {
+    let numerics: string = "";
+
+    for (const match of row.matchAll(pattern)) {
+      if (isNaN(Number(match[1]))) {
+        numerics += Number(wordMap.indexOf(match[1]));
+      } else {
+        numerics += Number(match[1]);
+      }
+    }
+
+    switch (numerics.length) {
+      case 0:
+        break;
+      case 1:
+        sum += Number(numerics[0] + numerics[0]);
+        break;
+      default:
+        sum += Number(numerics[0] + numerics[numerics.length - 1]);
+        break;
+    }
+  }
+
+  return sum;
+};
+
+const solution = solver(INPUT);
+console.log(solution);
